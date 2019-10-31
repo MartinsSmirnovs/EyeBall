e83f02655ee04aabaa3049fb7f9d5a570f09ac8c
Onii-Chaan <martinnc@inbox.lv>
2019-10-31 16:50:19 +0200

    31.10

--- shiftCode.ino
+++ shiftCode.ino
@@ -13,25 +13,45 @@ uint16_t potXVal;
 uint32_t oldTimeInt;
 bool buttonPress;
 
-uint16_t animationArr[50][3]{
+const uint8_t aniLen = 99;
+
+uint16_t animationArr[aniLen][3]{
     {3,3,200},
     {3,4,1000},
     {2,4,500},
     {1,5,100},
+    {2,4,400},
     {3,3,50},
     {3,2,50},
-    {4,1,400},
+    {4,1,4000},
     {5,1,500},
+    {4,2,500},
+    {3,3, 100},
+    {4,3,200},
+    {5,3,500},
     {6,3,400},
     {6,4,500},
+    {5,3,100},
+    {4,3,200},
+    {3,3,100},
     {3,4,100},
+    {2,3,240},
+    {1,2,500},
     {0,2,200},
+    {0,3, 100},
     {0,4,500},
     {0,5,100},
     {0,6,500},
-    {2,7,50},
+    {1,6,100},
+    {2,6,50},
+    {3,5,100},
+    {4,4, 600},
     {5,4,1000},
+    {5,3,200}, 
+    {4,2,700},
+    {3,2,250},
     {2,1,300},
+    {2,2,100},
     {3,3,400},
     {4,4,300},
     {5,5,200},
@@ -39,30 +59,61 @@ uint16_t animationArr[50][3]{
     {6,4,1000},
     {6,5,800},
     {6,6,200},
+    {6,5,460},
+    {6,4,900},
+    {6,3,600},
     {6,2,300},
+    {5,3,100},
+    {4,4,234},
+    {3,5,1000},
+    {2,5,600},
+    {2,6,350},
     {1,6,100},
+    {1,5,600},
+    {1,4,400},
+    {1,3,200},
     {0,2,400},
+    {1,3,200},
     {2,4,600},
-    {4,7,400},
-    {5,7,400},
+    {2,5,100},
+    {4,6,400},
+    {5,6,4000},
     {6,5,150},
+    {5,4,500},
+    {4,4,340},
     {3,3,250},
     {2,3,600},
     {3,3,400},
     {3,2,1000},
     {3,1,230},
+    {3,2,1000},
     {3,3,500},
+    {4,2,500},
     {5,1,100},
+    {5,0,1000},
+    {4,0,600},
     {3,1,500},
+    {2,1,100},
     {1,2,100},
+    {1,1,100},
     {0,2,60},
-    {2,7,30},
-    {4,7,500},
-    {5,7,100},
-    {2,7,100},
+    {1,2,100},
+    {1,3,400},
+    {1,4,500},
+    {2,5,600},
+    {2,6,30},
+    {3,6,50},
+    {4,6,500},
+    {5,6,100},
+    {4,6,1000},
+    {3,6,1000},
+    {2,6,100},
     {3,5,100},
+    {3,4,1000},
     {3,3,50},
+    {4,2,400},
     {5,3,230},
+    {4,3,1000},
     {3,3,1000}
 };
 
@@ -108,7 +159,7 @@ void loop() {
   }
 }
 
-uint8_t animationCounter;
+uint16_t animationCounter;
 uint32_t animationMillis;
 void animationDrive(){
     if(millis()-animationMillis >= animationArr[animationCounter][2]){
@@ -116,7 +167,7 @@ void animationDrive(){
         potXVal = animationArr[animationCounter][0];
         potYVal = animationArr[animationCounter][1];
         animationCounter ++;
-        if(animationCounter >=50){
+        if(animationCounter >=aniLen){
             animationCounter = 0;
         }
     }
@@ -141,7 +192,7 @@ uint8_t oldTimesToBlink;
 bool blinkDown;
 void blink(){//atbilst par mirkšķināšanu
     if(millis()-blinkTimeInt>=randomBlinkInt){//tiek iestatītas miršķināšanas random vērtības'
-        randomBlinkInt = random(3000, 5000);
+        randomBlinkInt = random(2000, 4000);
         blinkDown = true;
         topPartCounter = 0;
         botPartCounter = 7;
